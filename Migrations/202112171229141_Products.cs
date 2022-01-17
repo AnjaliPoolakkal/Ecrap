96833495f693d30184d5eff3887c85c8899bee62
apoolakkal <apoolakkal@SUYCOKHWDSK236>
2022-01-17 14:44:34 +0530

    Add project files.

---
+++ Migrations/202112171229141_Products.cs
@@ -0,0 +1,28 @@
+namespace Ecrap.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Products : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Pname = c.String(nullable: false),
+                        Description = c.String(nullable: false),
+                        price = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ecrap.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "product name is required")]
+        [Display(Name = "product name")]
+        public string Pname { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "price is required")]
+
+        public string price { get; set; }
+
+
+    }
+}
